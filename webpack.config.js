ad7af50064bc39d0258988c325f02e7069303e46
Frederic Heem <frederic.heem@gmail.com>
2017-05-22 21:56:45 +0100

    build lib without the deps

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const path = require('path');
+const glob = require('glob');
+const webpack = require('webpack');
+
+const OUT_PATH = path.resolve('./build');
+const PUBLIC_PATH = '/assets/';
+const IS_DEV = process.env.LMD_ENV === 'development';
+const IS_PROD = process.env.LMD_ENV === 'production';
+
+const LIFECYCLE_EVENT = process.env.npm_lifecycle_event;
+if (LIFECYCLE_EVENT == 'test' || LIFECYCLE_EVENT == 'test:watch') {
+  process.env.BABEL_ENV = 'test';
+}
+
+module.exports = [{
+  name: 'components',
+  entry: {
+    button: [path.resolve('./src/button/index.js')],
+    //base: [path.resolve('./index.js')],
+  },
+
+  externals: ['react', 'glamorous', 'glamour'],
+  output: {
+    path: OUT_PATH,
+    publicPath: PUBLIC_PATH,
+    filename: 'lmd.[name].' + (IS_PROD ? 'min.' : '') + 'js',
+    //libraryTarget: 'umd',
+    library: ['ldc', '[name]'],
+  },
+  devServer: {
+    disableHostCheck: true,
+  },
+  devtool: IS_DEV ? 'source-map' : false,
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        cacheDirectory: true,
+      },
+    }],
+  },
+  plugins: [
+  ],
+}
+];
