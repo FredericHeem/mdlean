b063761a83b73826a603db4bde2ddfb4c52a02aa
Frederic Heem <frederic.heem@gmail.com>
2017-05-23 22:27:38 +0100

    add theme.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import button from "./button";
+import theme from './theme';
 
 const context = {
-  theme: {
-    palette: {
-      backgroundColor: "#ffffff",
-      textPrimary: "#000000",
-      textPrimaryOnPrimary: "#000000",
-      textPrimaryOnAccent: "#000000",
-      primary1: "#00bcd4",
-      primary3: "#bdbdbd",
-      accent1: "#ff4081",
-      accent2: "#ff4081",
-      borderColor: "#e0e0e0"
-    }
-  }
+  theme: theme()
 };
 
 const Button = button(context);
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export default function() {
+   return {
+    palette: {
+      backgroundColor: "#ffffff",
+      textPrimary: "#000000",
+      textPrimaryOnPrimary: "#000000",
+      textPrimaryOnAccent: "#000000",
+      primary1: "#00bcd4",
+      primary3: "#bdbdbd",
+      accent1: "#ff4081",
+      accent2: "#ff4081",
+      borderColor: "#e0e0e0"
+    }
+  };
+}
