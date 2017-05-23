3e6f9672034c6bd65f3f28e6463aea0e22913598
Frederic Heem <frederic.heem@gmail.com>
2017-05-24 00:41:25 +0100

    add dark and light for primary

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,15 @@
 export default function() {
    return {
     palette: {
+      primaryDark: "#455A64",
       primary: "#607D8B",
-      accent: "#7C4DFF",
+      primaryLight: "#CFD8DC",
+      accent: "#7C4DFF",  
       background: "#ffffff",
       textPrimary: "#000000",
       textPrimaryOnPrimary: "#ffffff",
       textPrimaryOnAccent: "#ffffff",
+      textSecondary: "#757575",
       borderColor: "#BDBDBD"
     }
   };
