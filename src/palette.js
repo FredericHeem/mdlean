7c5e33f15e75fcb2a64f5437828d72a0bf8a3ae5
Frederic Heem <frederic.heem@gmail.com>
2019-06-22 19:31:30 +0100

    forgot palette.js

---
+++ src/palette.js
@@ -0,0 +1,14 @@
+export default function() {
+  return {
+    primaryDark: "#455A64",
+    primary: "#607D8B",
+    primaryLight: "#CFD8DC",
+    accent: "#7C4DFF",
+    background: "#ffffff",
+    textPrimary: "#000000",
+    textPrimaryOnPrimary: "#ffffff",
+    textPrimaryOnAccent: "#ffffff",
+    textSecondary: "#757575",
+    borderColor: "#BDBDBD"
+  };
+}
